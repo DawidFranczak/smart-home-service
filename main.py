ccf7ec1a2491caf6fc1f8c49514e28fa82c25c21
Dawid Franczak <daw.franczak@gmail.com>
2025-10-16 16:09:19 +0200

    Add middleware for validating service-to-service token

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from app.lifespan import startup_event
 from app.router.dummy import router as dummy_router
 from app.router.temperature import router as temperature_router
 from app.router.humidity import router as humidity_router
+from app.middleware.check_auth_token import check_auth_token
 
 app = FastAPI(lifespan=startup_event)
 
 app.include_router(dummy_router)
 app.include_router(temperature_router)
 app.include_router(humidity_router)
+
+
+app.middleware("http")(check_auth_token)
 
 
 @app.get("/")
