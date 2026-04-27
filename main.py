69b43aaf253f0e9fef3a93e4e2ea58df19ea0804
Dawid Franczak <daw.franczak@gmail.com>
2026-04-27 20:11:21 +0200

    add prometheus and grafana for monitoring

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.lifespan import startup_event
 from app.router.dummy import router as dummy_router
 from app.router.measurement import router as measurement_router
+from prometheus_fastapi_instrumentator import Instrumentator
 
 origins = ["http://localhost:5173"]
+
 app = FastAPI(lifespan=startup_event)
+Instrumentator().instrument(app).expose(app)
 app.include_router(dummy_router)
 app.include_router(measurement_router, prefix="/sensor/api/v1")
 
